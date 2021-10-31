54783c7bb479e5a4d028f8e241796e47d0379667
Molcsán Anna <molcsananna@gmail.com>
2021-10-31 08:42:48 +0100

    második commit

--- jatekGyar/jatekGyar/Form1.cs
+++ jatekGyar/jatekGyar/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using jatekGyar.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace jatekGyar
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+            createTimer.Enabled = true;
+            conveyorTimer.Enabled = true;
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            Left = -Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int legjobbra=0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left>legjobbra)
+                {
+
+                }
+            }
+            
         }
     }
 }
